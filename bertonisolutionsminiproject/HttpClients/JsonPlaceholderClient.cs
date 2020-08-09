d73167c0e411a094e6d4a8d2e518349819b35405
Paweł Hemperek <pawel+github@hemperek.pl>
2020-08-09 23:45:28 +0200

    Add some dummy logging when HTTP request fails

--- bertonisolutionsminiproject/HttpClients/JsonPlaceholderClient.cs
+++ bertonisolutionsminiproject/HttpClients/JsonPlaceholderClient.cs
@@ -1,4 +1,5 @@
 ﻿using bertonisolutionsminiproject.HttpClients.Models;
+using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,25 +13,42 @@ namespace bertonisolutionsminiproject.HttpClients
     public class JsonPlaceholderClient
     {
         private readonly HttpClient _client;
+        private readonly ILogger<JsonPlaceholderClient> _logger;
 
-        public JsonPlaceholderClient(HttpClient client)
+        public JsonPlaceholderClient(ILogger<JsonPlaceholderClient> logger, HttpClient client)
         {
             _client = client;
             _client.BaseAddress = new Uri("https://jsonplaceholder.typicode.com");
+            _logger = logger;
         }
+
 
         public async Task<IEnumerable<Album>> LoadAlbums()
         {
-            var responseString = await _client.GetStringAsync("albums");
-
-            return JsonSerializer.Deserialize<List<Album>>(responseString);
+            try
+            {
+                var responseString = await _client.GetStringAsync("albums");
+                return JsonSerializer.Deserialize<List<Album>>(responseString);
+            }
+            catch (Exception e)
+            {
+                _logger.LogError(e, "Something went wrong while fetching albums");
+                return new List<Album>();
+            }
         }
 
         public async Task<IEnumerable<Photo>> LoadPhotosForAlbum(int albumId)
         {
-            var responseString = await _client.GetStringAsync($"albums/{albumId}/photos");
-
-            return JsonSerializer.Deserialize<List<Photo>>(responseString);
+            try
+            {
+                var responseString = await _client.GetStringAsync($"albums/{albumId}/photos");
+                return JsonSerializer.Deserialize<List<Photo>>(responseString);
+            }
+            catch (Exception e)
+            {
+                _logger.LogError(e, "Something went wrong while fetching photos for album: @AlbumId", new { AlbumId = albumId });
+                return new List<Photo>();
+            }
         }
 
         public async Task<IEnumerable<Comment>> LoadComments(int photoId)
@@ -38,9 +56,16 @@ namespace bertonisolutionsminiproject.HttpClients
             var builder = new UriBuilder(new Uri(_client.BaseAddress, "comments"));
             builder.Query = $"postId={photoId}";
 
-            var responseString = await _client.GetStringAsync(builder.Uri);
-
-            return JsonSerializer.Deserialize<List<Comment>>(responseString);
+            try
+            {
+                var responseString = await _client.GetStringAsync(builder.Uri);
+                return JsonSerializer.Deserialize<List<Comment>>(responseString);
+            }
+            catch (Exception e)
+            {
+                _logger.LogError(e, "Something went wrong while fetching comments for photo: @PhotoId", new { PhotoId = photoId });
+                return new List<Comment>();
+            }
         }
     }
 }
